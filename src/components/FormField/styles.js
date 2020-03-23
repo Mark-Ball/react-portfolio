1a6ca1c763007e79217e15e50f5bb7ad6686366f
Mark-Ball <markball111@gmail.com>
2020-03-24 02:36:42 +1100

    fixed styling bugs in FormField css

--- src/components/FormField/styles.js
+++ src/components/FormField/styles.js
@@ -3,30 +3,34 @@ import styled from 'styled-components';
 const height = 50; // px
 const width = 300; // px
 
-export const InputBox = styled.div`
+export const InputBox = styled.label`
     display: inline-block;
     position: relative;
+    border: 1px solid grey;
+    border-radius: 4px;
     height: ${height}px;
     width: ${width}px;
 
     span {
         position: absolute;
-        top: 20px;
+        top: 15px;
         left: 20px;
         color: grey;
         transition: 0.2s;
     }
 
     input {
-        border: 1px solid grey;
-        border-radius: 5px;
-        height: ${height}px;
-        width: ${width}px;
-        outline-width: 0;
+        position: absolute;
+        top: 24px;
+        left: 10px;
+        border: none;
+        outline: none;
+        font-size: large;
 
         :focus + span {
             top: 5px;
             left: 10px;
+            font-size: .75em;
         }
     }
 `;
